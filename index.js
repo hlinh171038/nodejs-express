ec5d84d6e7ec3b688fe21d92ab3ede8f6c7d3e33
HL <hoanglinh171038@gmail.com>
2020-06-24 12:48:39 +0700

    lesson 3: query paramater

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var app = express();
 var port =3000;
 app.set('view engine', 'pug');
 app.set('views', './views');
+var users= [
+    {id: '1' , name:'Thinh'},
+    {id:'2' , name:'Linh'}
+]
 app.get('/',function(request, response){
     response.render('index',{
         name:'AAAA'
@@ -10,12 +14,18 @@ app.get('/',function(request, response){
 });
 app.get('/users',function(require,response){
     response.render('users/index',{
-        users:[
-            {id: '1' , name:'Thinh'},
-            {id:'2' , name:'Linh'}
-        ]
+        users:users
     }); 
 });
+app.get('/users/search',function(req,res){
+    var q = req.query.q; // vì req.query ==>{q:"inh"} -> .q(key)
+    var usermark = users.filter(function(user){
+       return user.name.toLowerCase().indexOf(q.toLowerCase()) !==-1;
+    });
+    res.render('users/index',{
+        users:usermark
+    })
+})
 app.listen(port, function(){
      console.log('Server listenning on port'+port);
 });
