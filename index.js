06fc56a353bd2e12ed2ebb0022be866365c2acb9
HL <hoanglinh171038@gmail.com>
2020-06-25 14:59:09 +0700

    lesson 4:method POST

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 var port =3000;
 app.set('view engine', 'pug');
 app.set('views', './views');
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 var users= [
     {id: '1' , name:'Thinh'},
     {id:'2' , name:'Linh'}
@@ -26,6 +28,13 @@ app.get('/users/search',function(req,res){
         users:usermark
     })
 })
+app.get('/users/create', function(req, res){
+    res.render('users/create');
+});
+app.post('/users/create',function(req , res){
+    users.push(req.body);   // method của request lấy nội dung post
+    res.redirect('/users') // method của response ddeew chuyển hướng
+});
 app.listen(port, function(){
      console.log('Server listenning on port'+port);
 });
