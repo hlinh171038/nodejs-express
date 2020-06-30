d80e42a9128a55b410d674ce957de02b9f4b99a4
HL <hoanglinh171038@gmail.com>
2020-06-30 14:57:10 +0700

    lesson 6: database

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var express= require('express');
+var low = require('lowdb');
+var FileSync = require('lowdb/adapters/FileSync');
+var adapter = new FileSync('db.json');
+db = low(adapter);
 var app = express();
 var port =3000;
+db.defaults({ users: []})
+  .write()
 app.set('view engine', 'pug');
 app.set('views', './views');
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-var users= [
-    {id: '1' , name:'Thinh'},
-    {id:'2' , name:'Linh'}
-]
+
 app.get('/',function(request, response){
     response.render('index',{
         name:'AAAA'
@@ -16,14 +19,14 @@ app.get('/',function(request, response){
 });
 app.get('/users',function(require,response){
     response.render('users/index',{
-        users:users
+        users:db.get('users').value()
     }); 
 });
 app.get('/users/search',function(req,res){
     var q = req.query.q; // vì req.query ==>{q:"inh"} -> .q(key)
-    var usermark = users.filter(function(user){
+    var usermark = db.get('users').filter(function(user){
        return user.name.toLowerCase().indexOf(q.toLowerCase()) !==-1;
-    });
+    }).write();
     res.render('users/index',{
         users:usermark
     })
@@ -32,7 +35,7 @@ app.get('/users/create', function(req, res){
     res.render('users/create');
 });
 app.post('/users/create',function(req , res){
-    users.push(req.body);   // method của request lấy nội dung post
+    db.get('users').push(req.body).write();   // method của request lấy nội dung post
     res.redirect('/users') // method của response ddeew chuyển hướng
 });
 app.listen(port, function(){
