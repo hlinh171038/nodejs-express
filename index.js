75165fa7149f20c8f64302861315707fa01c3cf5
HL <hoanglinh171038@gmail.com>
2020-07-01 15:01:51 +0700

    lesson-7: su dung trang view( route paramater,shortid)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express= require('express');
 var low = require('lowdb');
+var shortid= require('shortid');
 var FileSync = require('lowdb/adapters/FileSync');
 var adapter = new FileSync('db.json');
 db = low(adapter);
@@ -32,12 +33,22 @@ app.get('/users/search',function(req,res){
     })
 })
 app.get('/users/create', function(req, res){
-    res.render('users/create');
+    res.render('users/create'); //render khong co dau gach /
 });
 app.post('/users/create',function(req , res){
+    req.body.id = shortid.generate();      // thư viện shortid tạ ra id ngẫu nhiên
     db.get('users').push(req.body).write();   // method của request lấy nội dung post
     res.redirect('/users') // method của response ddeew chuyển hướng
 });
+app.get('/users/:userId', function(req, res){
+    var id = req.params.userId;
+    //console.log(typeof id);
+    var user =  db.get('users').find({id: id}).value();
+    //console.log(user);
+    res.render('users/view',{
+       user:user
+    })
+})
 app.listen(port, function(){
      console.log('Server listenning on port'+port);
 });
